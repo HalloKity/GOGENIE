67c09a9b3c1d61b85ef924adc5d6a8ed0aa0d29f
YAEJIN-JEONG <wls7367@naver.com>
2022-12-05 16:09:23 +0900

    [YJ] feat : create spring boot project

---
+++ src/main/java/com/kt/rookie/GogenieApplication.java
@@ -0,0 +1,13 @@
+package com.kt.rookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogenieApplication.class, args);
+	}
+
+}
