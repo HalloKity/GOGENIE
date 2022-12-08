f977149c9f59adb394b4cd6f61eaa0e3c7d22432
YAEJIN-JEONG <wls7367@naver.com>
2022-12-08 14:09:09 +0900

    [YJ] : feat : reservation VO

---
+++ src/main/java/com/kt/rookie/model/vo/ReservationVO.java
@@ -0,0 +1,60 @@
+package com.kt.rookie.model.vo;
+
+public class ReservationVO {
+	private String reservationId;
+	private String reserveDate;
+	private String stationId;
+	private String busId;
+	
+	public ReservationVO() {
+		super();
+	}
+
+	public ReservationVO(String reservationId, String reserveDate, String stationId, String busId) {
+		super();
+		this.reservationId = reservationId;
+		this.reserveDate = reserveDate;
+		this.stationId = stationId;
+		this.busId = busId;
+	}
+
+	public String getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(String reservationId) {
+		this.reservationId = reservationId;
+	}
+
+	public String getReserveDate() {
+		return reserveDate;
+	}
+
+	public void setReserveDate(String reserveDate) {
+		this.reserveDate = reserveDate;
+	}
+
+	public String getStationId() {
+		return stationId;
+	}
+
+	public void setStationId(String stationId) {
+		this.stationId = stationId;
+	}
+
+	public String getBusId() {
+		return busId;
+	}
+
+	public void setBusId(String busId) {
+		this.busId = busId;
+	}
+
+	@Override
+	public String toString() {
+		return "ReservationVO [reservationId=" + reservationId + ", reserveDate=" + reserveDate + ", stationId="
+				+ stationId + ", busId=" + busId + "]";
+	}
+	
+	
+}
