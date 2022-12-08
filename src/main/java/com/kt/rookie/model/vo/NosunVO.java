de8899d210475c305ad4bbf62e84143ea186daad
YAEJIN-JEONG <wls7367@naver.com>
2022-12-08 11:20:18 +0900

    [YJ] feat : project package

---
+++ src/main/java/com/kt/rookie/model/vo/NosunVO.java
@@ -0,0 +1,19 @@
+package com.kt.rookie.model.vo;
+
+public class NosunVO {
+	private String stationId;
+	private String commuteId;
+	private String busId;
+	private String line;
+	private String mainPlace;
+	private String detailPlace;
+	private String departureTime;
+	private Double latitue;
+	private Double longitude;
+	private String carNum;
+	private String phone;
+	private String officePlace;
+	private String officeTime;
+	
+	
+}
